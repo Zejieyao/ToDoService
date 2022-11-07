1be6aca180c447b89ad931d9a48ea96901bfe107
Zejie Yao <ZYao3@slb.com>
2022-11-07 14:54:39 +0800

    feat: get id in todoitem-detail component

--- ToDoFrontEnd/src/app/todo/todoitem-detail/todoitem-detail.component.ts
+++ ToDoFrontEnd/src/app/todo/todoitem-detail/todoitem-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../service/todo.service';
 import { ToDoItem } from '../../model/ToDoItem';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todoitem-detail',
@@ -13,8 +14,12 @@ export class TodoitemDetailComponent implements OnInit {
     return this.todoService.currentTodoItem();
   }
 
-  constructor(public todoService: TodoService) { }
+  constructor(
+    public todoService: TodoService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
   }
 }
